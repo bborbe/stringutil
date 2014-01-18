c4726443d95db3c39e7aa64ebcadd777f9a4850b
Benjamin Borbe <bborbe@rocketnews.de>
2014-01-18 14:14:40 +0100

    add stringutil

---
+++ stringutil.go
@@ -0,0 +1,44 @@
+package stringutil
+
+import (
+	"strings"
+)
+
+func StringAfter(content string, find string) string {
+	if len(find) == 0 {
+		return content
+	}
+	pos := strings.Index(content, find)
+	if pos == -1 {
+		return ""
+	}
+	return content[pos+len(find):]
+}
+
+func StringBefore(content string, find string) string {
+	if len(find) == 0 {
+		return content
+	}
+	pos := strings.Index(content, find)
+	if pos == -1 {
+		return ""
+	}
+	return content[:pos]
+}
+
+func Trim(content string) string {
+	runes := []rune(content)
+	if len(runes) > 0 {
+		if trimableChar(runes[0]) {
+			return Trim(content[1:])
+		}
+		if trimableChar(runes[len(content)-1]) {
+			return Trim(content[:len(content)-1])
+		}
+	}
+	return content
+}
+
+func trimableChar(c rune) bool {
+	return c == ' ' || c == '\n' || c == '\r'
+}
---
+++ stringutil_test.go
@@ -0,0 +1,142 @@
+package stringutil
+
+import (
+	"fmt"
+	. "github.com/bborbe/assert"
+	"testing"
+)
+
+type TestStringAfterData struct {
+	Content string
+	Find    string
+	Result  string
+}
+
+var TestStringAfterDatas = []TestStringAfterData{
+	{
+		Content: "",
+		Find:    "",
+		Result:  "",
+	},
+	{
+		Content: "test",
+		Find:    "",
+		Result:  "test",
+	},
+	{
+		Content: "test",
+		Find:    "test",
+		Result:  "",
+	},
+	{
+		Content: "bla:test",
+		Find:    "bla:",
+		Result:  "test",
+	},
+	{
+		Content: "test",
+		Find:    "bla:",
+		Result:  "",
+	},
+}
+
+func TestStringAfter(t *testing.T) {
+	for i, testdata := range TestStringAfterDatas {
+		result := StringAfter(testdata.Content, testdata.Find)
+		err := AssertThat(result, Is(testdata.Result).Message(fmt.Sprintf("result wrong in testcase %d!", i+1)))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+type TestStringBeforeData struct {
+	Content string
+	Find    string
+	Result  string
+}
+
+var TestStringBeforeDatas = []TestStringBeforeData{
+	{
+		Content: "",
+		Find:    "",
+		Result:  "",
+	},
+	{
+		Content: "test",
+		Find:    "",
+		Result:  "test",
+	},
+	{
+		Content: "test",
+		Find:    "test",
+		Result:  "",
+	},
+	{
+		Content: "foo:bar",
+		Find:    ":bar",
+		Result:  "foo",
+	},
+	{
+		Content: "test",
+		Find:    "bla:",
+		Result:  "",
+	},
+	{
+		Content: "a b c",
+		Find:    " ",
+		Result:  "a",
+	},
+}
+
+func TestStringBefore(t *testing.T) {
+	for i, testdata := range TestStringBeforeDatas {
+		result := StringBefore(testdata.Content, testdata.Find)
+		err := AssertThat(result, Is(testdata.Result).Message(fmt.Sprintf("result wrong in testcase %d!", i+1)))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+type TestTrimData struct {
+	Input    string
+	Expected string
+}
+
+var TestTrimDatas = []TestTrimData{
+	{
+		Input:    "",
+		Expected: "",
+	},
+	{
+		Input:    " hello ",
+		Expected: "hello",
+	},
+	{
+		Input:    " hello world ",
+		Expected: "hello world",
+	},
+	{
+		Input:    "\rhello world\r",
+		Expected: "hello world",
+	},
+	{
+		Input:    "\nhello world\n",
+		Expected: "hello world",
+	},
+	{
+		Input:    "\r \nhello world\n \r",
+		Expected: "hello world",
+	},
+}
+
+func TestTrim(t *testing.T) {
+	for i, testdata := range TestTrimDatas {
+		result := Trim(testdata.Input)
+		err := AssertThat(result, Is(testdata.Expected).Message(fmt.Sprintf("result wrong in testcase %d!", i+1)))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
